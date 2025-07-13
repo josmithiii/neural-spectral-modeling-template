64b6e0b6cee2b0507bdddfdfa36e149dd243650d
Julius Smith <2200853+josmithiii@users.noreply.github.com>
2025-07-12 19:18:18 -0700

    main: persistent_workers requires num_workers > 0

--- src/data/mnist_datamodule.py
+++ src/data/mnist_datamodule.py
@@ -74,7 +74,8 @@ class MNISTDataModule(LightningDataModule):
         """
         super().__init__()
 
-        self.persistent_workers = persistent_workers
+        # persistent_workers requires num_workers > 0
+        self.persistent_workers = persistent_workers and num_workers > 0
 
         # this line allows to access init params with 'self.hparams' attribute
         # also ensures init params will be stored in ckpt
